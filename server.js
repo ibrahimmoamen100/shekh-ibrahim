237be96630f2dcf7b49bbd7999cf180babbd759a
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2024-12-15 00:23:54 +0200

    new update

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const path = require('path');
 const multer = require('multer');
 const jwt = require('jsonwebtoken'); 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Enable CORS for development
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // Create necessary directories
 const publicDir = path.join(__dirname, 'public');
